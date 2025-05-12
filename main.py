da7abbc8a25a71e8264da5b1ce98d710c25b3e2f
julian-br <julian.berner@proton.me>
2025-05-12 10:46:15 +0000

    example spectrogramms

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from utils.create_spec import create_mel_spec
 from utils.plot_spec import plot_and_save_spec
 import numpy as np
 
-chord_spec= create_mel_spec("./data/chord-test.wav")
-single_note_spec= create_mel_spec("./data/single-notes.wav")
+guitar_performance_spec = create_mel_spec("./data/performance-cropped.wav")
+hummed_melodie_spec = create_mel_spec("./data/hummed-melodie.wav")
 
 
-plot_and_save_spec(chord_spec, "./output/chord-spec.png")
-np.save("./output/chord-spec.npy", chord_spec)
-
-plot_and_save_spec(single_note_spec, "./output/single-note-spec.png")
-np.save("./output/single-note-spec.npy", chord_spec)
+plot_and_save_spec(guitar_performance_spec, "./output/performance-cropped-spec.png")
+plot_and_save_spec(hummed_melodie_spec, "./output/hummed-melodie-spec.png")
--- utils/create_spec.py
+++ utils/create_spec.py
@@ -3,13 +3,12 @@ import numpy as np
 from scipy.signal import resample_poly
 
 def create_mel_spec(file_path, target_sr=22050):
-    # Load audio at original SR
     y, sr = librosa.load(file_path, sr=None)
 
     if sr != target_sr:
-      y = librosa.resample_poly(y, orig_sr=sr, target_sr=target_sr)
+      y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
 
-    # HiFi-GAN MelSpec Parameters
+  
     mel_spec = librosa.feature.melspectrogram(
         y=y,
         sr=target_sr,
@@ -19,7 +18,7 @@ def create_mel_spec(file_path, target_sr=22050):
         n_mels=80,
         fmin=0,
         fmax=8000,
-        power=1.0  # Wichtig: kein dB-Scaling
+        power=1.0
     )
 
     return mel_spec
